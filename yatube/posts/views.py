ad1519c28dc0d2ea24128aa05e235adb08f358a9
Vadosiy <dubrovskyvadim97@gmail.com>
2023-01-26 03:12:44 +0300

    исправление недочетов, проверка тестов

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -8,7 +8,7 @@ OUTPUT_POSTS = 10
 
 def index(request):
     """Выводим на страницу первые записи постов."""
-    posts = Post.objects.select_related('author')[OUTPUT_POSTS]
+    posts = Post.objects.select_related('author')[:OUTPUT_POSTS]
     template = 'posts/index.html'
     context = {'posts': posts}
     return render(request, template, context)
@@ -17,7 +17,7 @@ def index(request):
 def group_posts(request, slug):
     """Выводим на страницу первые записи групп."""
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.select_related('author')[OUTPUT_POSTS]
+    posts = group.posts.select_related('author')[:OUTPUT_POSTS]
     template = 'posts/group_list.html'
     context = {
         'group': group,
